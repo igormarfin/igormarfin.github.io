6cc6c3976def422b1d38e8aec7e35c6377df253f
debian <igor.marfin@unister.de>
2016-02-07 11:14:02 +0100

    new posts deploying

---
+++ pelicanconf.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Igor Marfin'
+SITENAME = u'Programmatic Statistics'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+PLUGIN_PATH = './plugins' 
+MARKUP = ('md', 'ipynb') 
+PLUGINS = ['ipynb','share_post'] 
+
+# my additional settings to the pelican config
+
+
+import os
+import sys
+
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+# Set the article URL.
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+
+
+#
+GOOGLE_PLUS_ID = '111481335270527541691'
+GOOGLE_PLUS_ONE = True 
+SEARCH_BOX = True 
+
+import os
+path = os.path.curdir
+THEME = '%s/theme' % path
